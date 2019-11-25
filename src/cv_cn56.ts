c2678b42f9ea54de361f54626387c63b03b50e04
Rodrigo Fernández Aragonés <rodrigo.fernandez@iot.bzh>
2019-11-25 09:54:44 +0100

    GNU General Public License > GPLv3.

--- src/cv_cn56.ts
+++ src/cv_cn56.ts
@@ -1,3 +1,29 @@
+/**
+ * @license
+ * Copyright (C) 2019 IoT.bzh Company
+ * Contact: https://www.iot.bzh/licensing
+ *
+ * This file is part of the civintec-reader module of the IoT.bzh project.
+ *
+ * $CV_BEGIN_LICENSE$
+ * Commercial License Usage
+ *  Licensees holding valid commercial IoT.bzh licenses may use this file in
+ *  accordance with the commercial license agreement provided with the
+ *  Software or, alternatively, in accordance with the terms contained in
+ *  a written agreement between you and The IoT.bzh Company. For licensing terms
+ *  and conditions see https://www.iot.bzh/terms-conditions. For further
+ *  information use the contact form at https://www.iot.bzh/contact.
+ *
+ * GNU General Public License Usage
+ *  Alternatively, this file may be used under the terms of the GNU General
+ *  Public license version 3. This license is as published by the Free Software
+ *  Foundation and appearing in the file LICENSE.GPLv3 included in the packaging
+ *  of this file. Please review the following information to ensure the GNU
+ *  General Public License requirements will be met
+ *  https://www.gnu.org/licenses/gpl-3.0.html.
+ * $CV_END_LICENSE$
+ */
+
 import { CV_Core, ICVWiegandMode } from "./cv_core";
 import { CV_Server, IreaderEvent } from "./cv_server";
 import { Observable, Subject } from "rxjs";
@@ -86,10 +112,24 @@ export class CV_CN56 extends CV_Core {
          * Once server is bound we set the 'listening events' function
          */
         this.socket.on('message', (data, rinfo) => {
+            this.active = true;
             // Send data to the general server subscribe()
             // to have all the readers activity
             this.server.setCN56ReaderEvent({ data, rinfo });
+
+            this.sendFrame(this.ip, this.port, Buffer.from('028000900800010100d2dd120e0b03','hex'));
         });
+
+        this.socket.on('error', (error) => {
+            this.active = false;
+            console.log(error);
+            console.log('Cannot listen on UPD for Civintec reader model CN56 ['+this.ip+']');
+        })
+        this.socket.on('connect', (error) => {
+            this.active = false;
+            console.log(error);
+            console.log('Connect');
+        })
 
         /**
          * Set wiegand mode
--- src/cv_core.ts
+++ src/cv_core.ts
@@ -1,5 +1,29 @@
-import { CV_Server } from './cv_server';
-import { CV_CT9 } from './cv_ct9';
+/**
+ * @license
+ * Copyright (C) 2019 IoT.bzh Company
+ * Contact: https://www.iot.bzh/licensing
+ *
+ * This file is part of the civintec-reader module of the IoT.bzh project.
+ *
+ * $CV_BEGIN_LICENSE$
+ * Commercial License Usage
+ *  Licensees holding valid commercial IoT.bzh licenses may use this file in
+ *  accordance with the commercial license agreement provided with the
+ *  Software or, alternatively, in accordance with the terms contained in
+ *  a written agreement between you and The IoT.bzh Company. For licensing terms
+ *  and conditions see https://www.iot.bzh/terms-conditions. For further
+ *  information use the contact form at https://www.iot.bzh/contact.
+ *
+ * GNU General Public License Usage
+ *  Alternatively, this file may be used under the terms of the GNU General
+ *  Public license version 3. This license is as published by the Free Software
+ *  Foundation and appearing in the file LICENSE.GPLv3 included in the packaging
+ *  of this file. Please review the following information to ensure the GNU
+ *  General Public License requirements will be met
+ *  https://www.gnu.org/licenses/gpl-3.0.html.
+ * $CV_END_LICENSE$
+ */
+
 
 /**
  * ICVWiegandMode : used to config Wiegand mode reader
